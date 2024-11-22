0278b8ea568c8f7802fd66abb4047e591a13b5a7
Rafael <166154431+RafaelChoza@users.noreply.github.com>
2024-11-21 20:15:53 -0600

    Add gener argument to class Persona

--- src/claseGit/ClaseGit.java
+++ src/claseGit/ClaseGit.java
@@ -5,14 +5,15 @@ import java.security.Permission;
 public class ClaseGit {
     public static void main(String[] args) {
         Persona persona = new Persona();
-        persona.metodo("Enrique", 33);
+        persona.metodo("Enrique", 33, "masculino");
     }
     static class Persona {
         static String name;
         static int edad;
+        static String genero;
 
-        static void metodo(String name, int edad) {
-            System.out.println("EL nombre de la persones es " + name + " y su edad es " + edad);
+        static void metodo(String name, int edad, String genero) {
+            System.out.println("EL nombre de la persones es " + name + " y su edad es " + edad + " y su genero es" + genero);
         }
     }
 }
